202e6dd7b7c06ae56903cf42c311840eff496e10
NeilChaudhari21 <neilwoodinville@gmail.com>
2026-05-19 16:00:34 -0700

    Add custom map location marker

--- src/components/WeatherMap.tsx
+++ src/components/WeatherMap.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
+import L from "leaflet";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 type WeatherMapProps = {
@@ -8,6 +9,18 @@ type WeatherMapProps = {
   longitude: number;
   label: string;
 };
+
+const locationPinIcon = L.divIcon({
+  className: "weather-map-pin",
+  html: `
+    <svg class="weather-map-pin-svg" viewBox="0 0 64 86" aria-hidden="true">
+      <path fill="currentColor" d="M32 0C14.3 0 0 14.3 0 32c0 24.1 32 54 32 54s32-29.9 32-54C64 14.3 49.7 0 32 0Zm0 47a15 15 0 1 1 0-30 15 15 0 0 1 0 30Z" />
+    </svg>
+  `,
+  iconSize: [34, 46],
+  iconAnchor: [17, 44],
+  popupAnchor: [0, -42],
+});
 
 export default function WeatherMap({
   latitude,
@@ -28,7 +41,7 @@ export default function WeatherMap({
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
+      <Marker icon={locationPinIcon} position={position}>
         <Popup>{label}</Popup>
       </Marker>
     </MapContainer>
